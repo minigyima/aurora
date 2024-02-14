b8a6c2be1d49a79ba178f739eab4d8ea0336ae55
Máté Gyimesi <gyim.mate@gmail.com>
2024-02-14 14:23:39 +0100

    Production builds

--- src/Commands/PrepareProductionCommand.php
+++ src/Commands/PrepareProductionCommand.php
@@ -5,6 +5,7 @@ namespace Minigyima\Aurora\Commands;
 use Illuminate\Console\Command;
 use Minigyima\Aurora\Concerns\VerifiesEnvironment;
 use Minigyima\Aurora\Support\ConsoleLogger;
+use Minigyima\Aurora\Support\StrClean;
 
 /**
  * PrepareProductionCommand - Command for preparing the production environment
@@ -35,12 +36,18 @@ class PrepareProductionCommand extends Command
     public function handle(): int
     {
         if (! self::runningInMercury()) {
-            ConsoleLogger::log_error('This command can only be run in the Mercury environment');
+            ConsoleLogger::log_error(
+                'This command can only be run in the Mercury environment',
+                'PrepareProductionCommand'
+            );
             return self::FAILURE;
         }
 
         if (! self::runningInProduction()) {
-            ConsoleLogger::log_error('This command can only be run in the production environment');
+            ConsoleLogger::log_error(
+                'This command can only be run in the production environment',
+                'PrepareProductionCommand'
+            );
             return self::FAILURE;
         }
 
@@ -48,26 +55,74 @@ class PrepareProductionCommand extends Command
 
         ConsoleLogger::log_trace('Checking if Cron is enabled...');
         if (! config('aurora.scheduler_enabled')) {
-            ConsoleLogger::log_warning('Cron is not enabled. Removing config...');
+            ConsoleLogger::log_warning('Cron is not enabled. Removing config...', 'PrepareProductionCommand');
             unlink('/etc/supervisor/conf.d/supervisord-20-cron.conf');
         }
 
-        ConsoleLogger::log_trace('Checking if Horizon is enabled...');
+        ConsoleLogger::log_trace('Checking if Horizon is enabled...', 'PrepareProductionCommand');
         if (! config('aurora.queue_enabled')) {
-            ConsoleLogger::log_warning('Horizon is not enabled. Removing config...');
+            ConsoleLogger::log_warning('Horizon is not enabled. Removing config...', 'PrepareProductionCommand');
             unlink('/etc/supervisor/conf.d/supervisord-30-queue.conf');
         }
 
-        ConsoleLogger::log_trace('Checking if Redis is enabled...');
+        ConsoleLogger::log_trace('Checking if Redis is enabled...', 'PrepareProductionCommand');
         if (! config('aurora.redis_enabled')) {
-            ConsoleLogger::log_warning('Redis is not enabled. Removing config...');
+            ConsoleLogger::log_warning('Redis is not enabled. Removing config...', 'PrepareProductionCommand');
             unlink('/etc/supervisor/conf.d/supervisord-40-redis.conf');
         }
 
-        ConsoleLogger::log_trace('Checking if Postgres is enabled...');
+        ConsoleLogger::log_trace('Checking if Postgres is enabled...', 'PrepareProductionCommand');
         if (! config('aurora.database_enabled')) {
-            ConsoleLogger::log_warning('Postgres is not enabled. Removing config...');
+            ConsoleLogger::log_warning('Postgres is not enabled. Removing config...', 'PrepareProductionCommand');
             unlink('/etc/supervisor/conf.d/supervisord-50-postgres.conf');
+        }
+
+        ConsoleLogger::log_trace('Checking if Soketi is enabled...', 'PrepareProductionCommand');
+        if (! config('aurora.sockets_enabled')) {
+            ConsoleLogger::log_warning('Soketi is not enabled. Removing config...', 'PrepareProductionCommand');
+            unlink('/etc/supervisor/conf.d/supervisord-70-soketi.conf');
+        } else {
+            ConsoleLogger::log_info('Soketi is enabled. Configuring...', 'PrepareProductionCommand');
+            $app_name = str_replace([' ', '-'], ['_', '_'], StrClean::clean(strtolower(config('app.name'))));
+            $app_id = config('broadcasting.connections.pusher.app_id');
+            $app_key = config('broadcasting.connections.pusher.key');
+            $app_secret = config('broadcasting.connections.pusher.secret');
+            $soketi_port = config('broadcasting.connections.pusher.options.port');
+
+            $soketi_config = [
+                'debug' => false,
+                'port' => $soketi_port,
+                'metrics' => [
+                    'enabled' => true,
+                    'port' => 9601,
+                    'driver' => 'prometheus',
+                    'prometheus' => [
+                        'prefix' => "{$app_name}_"
+                    ]
+                ],
+                'appManager.array.apps' => [
+                    [
+                        'id' => $app_id,
+                        'key' => $app_key,
+                        'secret' => $app_secret,
+                        'webhooks' => []
+                    ]
+                ]
+            ];
+
+            ConsoleLogger::log_trace('Writing Soketi config to /etc/soketi-config.json', 'PrepareProductionCommand');
+            $soketi_config_json = json_encode($soketi_config, JSON_PRETTY_PRINT);
+            file_put_contents('/etc/soketi-config.json', $soketi_config_json);
+            ConsoleLogger::log_success('Soketi config written to /etc/soketi-config.json', 'PrepareProductionCommand');
+
+            ConsoleLogger::log_trace('Configuring nginx port...', 'PrepareProductionCommand');
+            $nginx_config = file_get_contents('/etc/nginx/nginx.conf');
+            $replace_string = 'http://127.0.0.1:6001';
+            $replace_with = "http://127.0.0.1:$soketi_port";
+
+            $nginx_config = str_replace($replace_string, $replace_with, $nginx_config);
+            file_put_contents('/etc/nginx/nginx.conf', $nginx_config);
+            ConsoleLogger::log_success('Nginx port configured', 'PrepareProductionCommand');
         }
 
         ConsoleLogger::log_success('Production environment prepared', 'PrepareProductionCommand');
