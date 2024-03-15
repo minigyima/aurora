76163bb9f1601cb622f023e3d7d84c0763b85d98
Máté Gyimesi <gyim.mate@gmail.com>
2024-03-15 12:27:42 +0100

    fix: I'm dumb

--- src/Stubs/docker/app/mercury_kill.sh
+++ src/Stubs/docker/app/mercury_kill.sh
@@ -3,5 +3,5 @@
 if [ -f /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini ]; then
     rm /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
 fi
-killall supervisord 2>&1 /dev/null
-/usr/local/bin/php -d xdebug.mode=off /srv/www/artisan octane:stop 2>&1 /dev/null
+killall supervisord 1> /dev/null 2> /dev/null
+/usr/local/bin/php -d xdebug.mode=off /srv/www/artisan octane:stop 1> /dev/null 2> /dev/null
