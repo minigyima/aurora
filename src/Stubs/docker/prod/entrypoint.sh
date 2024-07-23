a582c5ae52cc093ce89b64f07f8685d987e965ef
Máté Gyimesi <gyim.mate@gmail.com>
2024-07-23 15:31:52 +0200

    fix: permission fixes

--- src/Stubs/docker/prod/entrypoint.sh
+++ src/Stubs/docker/prod/entrypoint.sh
@@ -2,6 +2,13 @@
 
 source /mercury/index.sh
 
+mkdir -p /srv/www/storage/app /srv/www/storage/framework/cache /srv/www/storage/framework/views
+mkdir -p /srv/www/storage/app/public /srv/www/storage/logs /srv/www/bootstrap/cache
+mkdir -p /srv/www/storage/api-docs /srv/www/storage/permission_cache /srv/www/storage/aurora
+
+chmod -f -R 777 /srv/www/storage/framework /srv/www/storage/logs /srv/www/bootstrap/cache /srv/www/storage/api-docs /srv/www/storage/permission_cache /srv/www/storage/aurora
+chmod 777 /srv/www/storage/app /srv/www/storage/app/public
+
 export POSTGRES_USER=$database_user
 export POSTGRES_PASSWORD=$database_password
 export POSTGRES_DB=$database_name
@@ -19,9 +26,6 @@ chown -R aurora:aurora /var/log/nginx
 
 chown root:aurora /etc/nginx/nginx.conf
 chmod 640 /etc/nginx/nginx.conf
-
-chmod -f -R 777 /srv/www/storage/framework /srv/www/storage/logs /srv/www/bootstrap/cache /srv/www/storage/api-docs /srv/www/storage/permission_cache /srv/www/storage/aurora
-chmod 777 /srv/www/storage/app /srv/www/storage/app/public
 
 # Why?
 chown -R redis:redis /redis-data
